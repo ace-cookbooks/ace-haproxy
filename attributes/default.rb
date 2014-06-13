00ff8a992b384d180e79e992c3d50b1496bd96a1
Ryan Schlesinger <ryan@instanceinc.com>
2014-06-13 11:58:25 -0700

    Force defaults for client/server timeout

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+force_default['haproxy']['client_timeout'] = '65s'
+force_default['haproxy']['server_timeout'] = '65s'
---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name             "ace-haproxy"
+maintainer       "Ace of Sales"
+maintainer_email "cookbooks@aceofsales.com"
+license          "Apache 2.0"
+description      "Configures haproxy for aceofsales"
+version          "1.0.0"
